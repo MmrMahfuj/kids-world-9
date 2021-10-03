7bcdeee733d2377714ef301ebd22ba596c6cf964
MmrMahfuj <mahfuj.mmr3@gmail.com>
2021-10-04 03:00:30 +0600

    start project

---
+++ src/components/Header/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+import './Header.css';
+
+const Header = () => {
+    const activeStyle = {
+        fontWeight: "bold",
+        color: "red"
+    }
+    return (
+        <Navbar bg="dark" variant="dark">
+            <Container>
+                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                <Nav className="me-auto">
+                    <NavLink className="nav-link" activeStyle={activeStyle} to="/home">Home</NavLink>
+                    <NavLink className="nav-link" activeStyle={activeStyle} to="/services">Services</NavLink>
+                    <NavLink className="nav-link" activeStyle={activeStyle} to="/about">About</NavLink>
+                </Nav>
+            </Container>
+        </Navbar>
+    );
+};
+
+export default Header;
